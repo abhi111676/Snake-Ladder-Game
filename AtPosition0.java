f3fc7e92e54eecf821f57b62b5516db3ce9786f2
abhi111676 <abhi111676@gmail.com>
2021-07-09 05:15:43 +0530

    Here snake&ladder game is played by a single player at position 0

---
+++ AtPosition0.java
@@ -0,0 +1,13 @@
+
+public class AtPosition0 {
+
+	public static void main(String[] args) {
+
+		// variable
+		int playerPosition = 0;
+
+		System.out.println("Welcome to the Game of SNAKE & LADDER");
+		System.out.println("Starting Position of the Player = " + playerPosition);
+
+	}
+}
